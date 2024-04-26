b12d786dca3d424e1fb4998ad1db96bc2bb60986
amir-38 <a_saruxanov_38@mail.ru>
2024-04-27 02:40:33 +0400

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,72 @@
+const productData = [
+  { 
+    id: 1, 
+    imageUrl: "./assets/images/001.jpg",
+    title: "Product 1",
+    price: 10.99,
+    description: "Description of Product 1"
+  },
+  { 
+    id: 2, 
+    imageUrl: "./assets/images/002.jpg",
+    title: "Product 2",
+    price: 19.99,
+    description: "Description of Product 2"
+  },
+  { 
+    id: 3, 
+    imageUrl: "./assets/images/003.jpg",
+    title: "Product 3",
+    price: 15.49,
+    description: "Description of Product 3"
+  },
+  { 
+    id: 4, 
+    imageUrl: "./assets/images/004.jpg",
+    title: "Product 4",
+    price: 22.99,
+    description: "Description of Product 4"
+  },
+  { 
+    id: 5, 
+    imageUrl: "./assets/images/005.jpg",
+    title: "Product 5",
+    price: 12.99,
+    description: "Description of Product 5"
+  },
+  { 
+    id: 6, 
+    imageUrl: "./assets/images/006.jpg",
+    title: "Product 6",
+    price: 18.99,
+    description: "Description of Product 6"
+  },
+  { 
+    id: 7, 
+    imageUrl: "./assets/images/005.jpg",
+    title: "Product 7",
+    price: 14.99,
+    description: "Description of Product 7"
+  },
+  { 
+    id: 8, 
+    imageUrl: "./assets/images/005.jpg",
+    title: "Product 8",
+    price: 9.99,
+    description: "Description of Product 8"
+  },
+  { 
+    id: 9, 
+    imageUrl: "./assets/images/005.jpg",
+    title: "Product 9",
+    price: 17.99,
+    description: "Description of Product 9"
+  },
+  { 
+    id: 10, 
+    imageUrl: "./assets/images/005.jpg",
+    title: "Product 10",
+    price: 16.99,
+    description: "Description of Product 10"
+  }
+];
